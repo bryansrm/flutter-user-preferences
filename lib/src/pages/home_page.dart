0c7a2186b3b07dfb58cbd5aa5d87e5efac8f70db
Bryan Rodriguez <bsteven1993@gmail.com>
2020-08-07 17:20:35 -0500

    se agrega drawer en home

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:app_user_preferences/src/pages/settings_page.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -10,16 +11,60 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Preferencias de usuario'),
       ),
+      drawer: _crearMenu(context),
       body: Column(
-          children: [
-            Text('Color secundario:'),
-            Divider(),
-            Text('Género:'),
-            Divider(),
-            Text('Nombre de usuario:'),
-            Divider(),
-          ],
-        )
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('Color secundario:'),
+          Divider(),
+          Text('Género:'),
+          Divider(),
+          Text('Nombre de usuario:'),
+          Divider(),
+        ],
+      )
+    );
+  }
+
+  Drawer _crearMenu(BuildContext context){
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          DrawerHeader(
+            child: Container(),
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/menu-img.jpg'),
+                fit: BoxFit.cover
+              )
+            ),
+          ),
+          ListTile(
+            leading: Icon( Icons.home, color: Colors.blue),
+            title: Text('Home'),
+            onTap: () => Navigator.pop(context),
+          ),
+          ListTile(
+            leading: Icon( Icons.person, color: Colors.blue),
+            title: Text('Person'),
+            onTap: (){},
+          ),
+          ListTile(
+            leading: Icon( Icons.warning, color: Colors.blue),
+            title: Text('Alertas'),
+            onTap: (){},
+          ),
+          ListTile(
+            leading: Icon( Icons.settings, color: Colors.blue),
+            title: Text('Ajustes'),
+            onTap: (){
+              Navigator.pop(context);
+              Navigator.pushReplacementNamed(context, SettingsPage.routName);
+            }
+          ),
+        ],
+      )
     );
   }
 }
